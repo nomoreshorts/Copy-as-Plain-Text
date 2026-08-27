2d5251be1e94000cacc5f789a876d4b12c7e8e80
nomoreshorts <288374482+nomoreshorts@users.noreply.github.com>
2026-08-27 23:35:32 +0800

    Implement displaying plain text from the current tab in popup

--- src/background/background.mts
+++ src/background/background.mts
@@ -1,8 +1,21 @@
-import { copyAsPlainText } from './copyAsPlainText.mjs'
+import { RuntimeMessage } from '../types';
+import { copyAsPlainText, getSelectedText } from './copyAsPlainText.mjs'
 chrome.commands.onCommand.addListener(async command => {
   switch (command) {
     case 'copy-as-plain-text': {
       await copyAsPlainText()
+      break;
+    }
+  }
+})
+
+chrome.runtime.onMessage.addListener(async (message: RuntimeMessage, sender, res) => {
+  if (message.target !== "background") {
+    return;
+  }
+  switch (message.type) {
+    case 'get-selected': {
+      res(await getSelectedText())
       break;
     }
   }
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -12,37 +12,64 @@ class CopyUI {
     if (!this.clipboardBtn) {
       console.warn("Clipboard radio not found")
     }
+    if (!this.copyBtn) {
+      console.warn("Copy button not found")
+    }
+    this.getPlainTextFromSelection()
 
-    this.currentTabBtn?.addEventListener('click', e => {
-      if (!this.plainTextRepresentation) {
-        console.warn("Plain text field not found")
+    this.currentTabBtn?.addEventListener('click', this.getPlainTextFromSelection.bind(this))
+    this.clipboardBtn?.addEventListener('click', this.getPlainTextFromClipboard.bind(this))
+    this.copyBtn?.addEventListener('click', async e => {
+      e.preventDefault()
+      if (!this.plainTextRepresentation || this.plainTextRepresentation.value === '') {
         return;
       }
-      this.plainTextRepresentation.value = ''
-      /** @todo todo */
+      await navigator.clipboard.writeText(this.plainTextRepresentation.value)
     })
-    this.clipboardBtn?.addEventListener('click', async e => {
-      if (!this.plainTextRepresentation) {
-        console.warn("Plain text field not found")
-        return;
-      }
-      this.plainTextRepresentation.value = ''
-      let clipboardValue
-      try {
+  }
+  private async getPlainTextFromSelection() {
+    if (!this.plainTextRepresentation) {
+      console.warn("Plain text field not found")
+      return;
+    }
+    this.plainTextRepresentation.value = ''
+    const getSelectionMsg:RuntimeMessage = {
+      target: "background",
+      type: "get-selected",
+      info: ""
+    }
+
+    const res = await chrome.runtime.sendMessage(getSelectionMsg) as string|null|false
+    if (res === false) {
+      this.plainTextRepresentation.placeholder = "Cannot access current URL"
+    } else if (res === null || res === '') {
+      this.plainTextRepresentation.placeholder = "(No text selected)"
+    } else {
+      this.plainTextRepresentation.value = res
+    }
+  }
+
+  private async getPlainTextFromClipboard() {
+    if (!this.plainTextRepresentation) {
+      console.warn("Plain text field not found")
+      return;
+    }
+    this.plainTextRepresentation.value = ''
+    let clipboardValue
+    try {
+      clipboardValue = await navigator.clipboard.readText()
+    } catch {
+      this.plainTextRepresentation.placeholder = "Accept the clipboard read permission prompt to use this feature."
+      const promptRes = this.requestClipboardRead()
+      if (await promptRes) {
+        this.plainTextRepresentation.removeAttribute('placeholder')
         clipboardValue = await navigator.clipboard.readText()
-      } catch {
-        this.plainTextRepresentation.placeholder = "Accept the clipboard read permission prompt to use this feature."
-        const promptRes = this.requestClipboardRead()
-        if (await promptRes) {
-          this.plainTextRepresentation.removeAttribute('placeholder')
-          clipboardValue = await navigator.clipboard.readText()
-        } else {
-          return;
-        }
+      } else {
+        return;
       }
+    }
 
-      this.plainTextRepresentation.value = clipboardValue
-    })
+    this.plainTextRepresentation.value = clipboardValue
   }
 
   private async requestClipboardRead() {
