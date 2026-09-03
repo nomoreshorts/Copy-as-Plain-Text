2020f07f396b39bbd796ac81197ab03e2514951f
nomoreshorts <288374482+nomoreshorts@users.noreply.github.com>
2026-09-03 23:40:44 +0800

    last_interaction.ts: Create a new scope to prevent redefinition errors

--- src/content_scripts/last_interaction.ts
+++ src/content_scripts/last_interaction.ts
@@ -1,7 +1,9 @@
 import { CaPTWindow } from '../types'
-const captWindow = window as unknown as CaPTWindow
-captWindow.__lastActive = 0
-window.addEventListener('pointerdown', e => {
-  // we need this to know which was the last active frame and thus which frame to get selection from.
-  captWindow.__lastActive = Date.now()
-})
+{
+  const captWindow = window as unknown as CaPTWindow
+  captWindow.__lastActive = 0
+  window.addEventListener('pointerdown', e => {
+    // we need this to know which was the last active frame and thus which frame to get selection from.
+    captWindow.__lastActive = Date.now()
+  })
+}
